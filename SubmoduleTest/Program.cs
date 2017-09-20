86c64421a80cf0aab25522f15caa0130ade22e43
Matthew Kapteyn <matthewk@maple.ca>
2017-09-20 13:27:54 -0400

    cleanup

--- SubmoduleTest/Program.cs
+++ SubmoduleTest/Program.cs
@@ -12,6 +12,8 @@ namespace SubmoduleTest
             Test.HW_ALGO_SELECTOR("This_is_a_15_digit_imei_jk");
 
             Console.WriteLine("Made second change to SubmoduleTest");
+
+            Console.WriteLine("Made third change to SubmoduleTest");
         }
     }
 }
