71462f5d437312f09643ddbed0a72aceb96d9a22
Matthew Kapteyn <matthewk@maple.ca>
2017-09-20 09:37:53 -0400

    First commit of dummy project

---
+++ SubmoduleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Huawei_Unlock;
+
+namespace SubmoduleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Test.HW_ALGO_SELECTOR("This_is_a_15_digit_imei_jk");
+
+        }
+    }
+}
