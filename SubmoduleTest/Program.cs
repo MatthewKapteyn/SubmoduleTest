bb3163df5eb46bab684fefc72f6684b3e3f7804c
MatthewKapteyn <matthewk@maple.ca>
2018-03-01 14:03:45 -0500

    this commit we never want again (in the future)

--- SubmoduleTest/Program.cs
+++ SubmoduleTest/Program.cs
@@ -14,6 +14,8 @@ namespace SubmoduleTest
             Console.WriteLine("Made second change to SubmoduleTest");
 
             Console.WriteLine("Made third change to SubmoduleTest");
+			
+			// I've got a lovely bunch of cocnuts - we don't want this line - its terrible - but we don't know that yet
         }
     }
 }
